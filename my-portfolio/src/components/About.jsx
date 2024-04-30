56d864a9a609bf4f15b6f8d8ada25ffdd03476b4
pawelkozubal <31888831+pawelkozubal@users.noreply.github.com>
2024-04-30 23:50:01 +0100

    Initial commit

---
+++ my-portfolio/src/components/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react' 
+import aboutSvg from '../assets/about.svg';
+import SectionTitle from './SectionTitle';
+
+const About = () => {
+  return (
+    <section className='bg-white py-20 ' id='about'>
+        <div className='align-element grid md:grid-cols-2 items-center gap-16 '>
+            <img src={aboutSvg} alt="about section img" className='w-full h-64 ' /> 
+            <article>
+                <SectionTitle text='code and coffee' /> 
+                <p className='text-slate-600 mt-8 leading-loose'>  
+
+                Self tought web developer - passionate to make you life easier.
+
+                </p>
+            </article>
+        </div>
+    </section>
+  )
+}
+
+export default About
